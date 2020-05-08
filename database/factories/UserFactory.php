055bf8ec94cd5e7ad223791fb9d91456c6e5962e
daddyBai <133122451@qq.com>
2020-05-08 17:53:22 +0000

    填充用户和话题数据

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -5,6 +5,7 @@
 use App\Models\User;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Carbon\Carbon;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +19,19 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(User::class, function (Faker $faker) {
+    // 随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    // 传参为生成最大时间不超过，因为创建时间需永远比更改时间要早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
+        'email_verified_at' => $updated_at,
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'introduction' => $faker->sentence(),
         'remember_token' => Str::random(10),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
