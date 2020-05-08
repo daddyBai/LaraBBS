72acd803258879674a4bd4564940c9092c229729
daddyBai <133122451@qq.com>
2020-05-08 18:54:18 +0000

    用戶話題列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -39,4 +39,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
         'created_at' => 'datetime:Y-m-d H:i:s',
         'updated_at' => 'datetime:Y-m-d H:i:s',
     ];
+
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
