65e33977e9add44784c8e47531b08cc7fd04d527
daddyBai <133122451@qq.com>
2020-05-08 16:27:56 +0000

    上传头像

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -15,26 +15,35 @@
 
         <div class="card-body">
 
-          <form action="{{ route('users.update', $user->id) }}" method="post" accept-charset="UTF-8">
+          <form action="{{ route('users.update', $user->id) }}" method="post" accept-charset="UTF-8" enctype="multipart/form-data">
 
             {{ method_field('PUT') }}
             {{ csrf_field() }}
 
             @include('shared._error')
 
-            <div class="from-group">
+            <div class="form-group">
               <label for="name-field">用户名</label>
               <input type="text" class="form-control" name="name" id="name-field" value="{{ old('name', $user->name) }}">
             </div>
 
-            <div class="from-group">
+            <div class="form-group">
               <label for="email-field">邮 箱</label>
               <input type="text" class="form-control" name="email" id="email-field" value="{{ old('email', $user->email) }}">
             </div>
 
-            <div class="from-group">
+            <div class="form-group">
               <label for="introduction-field">个人简介</label>
-              <textarea type="text" class="form-control" name="introduction" id="introduction-field" value="{{ old('introduction', $user->introduction) }}"></textarea>
+              <textarea type="text" class="form-control" name="introduction" id="introduction-field" >{{ old('introduction', $user->introduction) }}</textarea>
+            </div>
+
+            <div class="form-group mb-4">
+              <label for="avatar-label">用户头像</label>
+              <input type="file" name="avatar" class="form-control-file">
+              @if($user->avatar)
+                <br>
+                <img src="{{ $user->avatar }}" alt="{{ $user->name }}" class="thumbnail img-responsive" width="200">
+              @endif
             </div>
 
             <div class="well well-sm">
