117f547045837012ca42ed1c1b3f36191044a344
daddyBai <133122451@qq.com>
2020-05-08 18:32:29 +0000

    分類話題列表

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -13,9 +13,13 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-      <!-- Left Side Of Navbar -->
+      {{-- todo, summerblue/laravel-active not support larvel V7.10.3 --}}
       <ul class="navbar-nav mr-auto">
-
+        <li class="nav-item active"><a class="nav-link" href="{{ route('topics.index') }}">话题</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('categories.show', 1) }}">分享</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('categories.show', 2) }}">教程</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('categories.show', 3) }}">问答</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('categories.show', 4) }}">公告</a></li>
       </ul>
 
       <!-- Right Side Of Navbar -->
