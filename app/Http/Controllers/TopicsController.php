ed325804ae31599ab573bf428383d4ac2badc1d8
daddyBai <133122451@qq.com>
2020-05-09 07:12:09 +0000

    修复 XSS 注入漏洞

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -40,6 +40,7 @@ class TopicsController extends Controller
     {
         $topic->fill($request->all());
         $topic->user_id = Auth::id();
+
         $topic->save();
         return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功 !');
     }
