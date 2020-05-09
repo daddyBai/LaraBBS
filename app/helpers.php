d89faefb261b41581f7f753b8bde82a5e0d9207c
daddyBai <133122451@qq.com>
2020-05-09 06:17:50 +0000

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -14,3 +14,9 @@ function order_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
 }
+
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\r|\r|\n+/', ' ', strip_tags($value)));
+    return \Illuminate\Support\Str::limit($excerpt, $length);
+}
