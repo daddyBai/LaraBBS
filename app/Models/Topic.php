9d297de677271db0e42c57815e059c5f73867689
daddyBai <133122451@qq.com>
2020-05-10 09:39:28 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -56,4 +56,10 @@ class Topic extends Model
     {
         return $this->hasMany(Reply::class);
     }
+
+    public function updatedReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
