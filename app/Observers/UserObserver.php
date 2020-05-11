013211037a84dc4badcc84be99479355ea317fb5
daddyBai <133122451@qq.com>
2020-05-11 09:54:04 +0000

    注册用户默认头像

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -18,4 +18,11 @@ class UserObserver
     {
         //
     }
+
+    public function saving(User $user)
+    {
+        if(empty($user->avatar)){
+            $user->avatar =  env('APP_URL','').'/uploads/avatars/default.png';
+        }
+    }
 }
