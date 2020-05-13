69be71eeda5b149cf1b132adc6c9bdeee816e0e2
daddyBai <133122451@qq.com>
2020-05-13 06:38:47 +0000

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('v1')->name('api.v1.')->group(function() {
-
+Route::prefix('v1')->namespace('Api')->name('api.v1.')->group(function() {
+    Route::post('verificationCodes', 'VerificationCodesController@store')
+        ->name('verificationCodes.store');
 });
 
 
