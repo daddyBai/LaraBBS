3d69b896f85d2992aaabe4ab0dc017d51eea4cde
daddyBai <133122451@qq.com>
2020-05-15 04:42:54 +0000

    修改话题

--- app/Http/Controllers/Api/TopicsController.php
+++ app/Http/Controllers/Api/TopicsController.php
@@ -6,7 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\Api\TopicRequest;
 use App\Http\Resources\TopicResource;
 use App\Models\Topic;
-use Illuminate\Http\Request;
 
 class TopicsController extends Controller
 {
@@ -16,6 +15,14 @@ class TopicsController extends Controller
         $topic->user_id = $request->user()->id;
         $topic->save();
 
+        return new TopicResource($topic);
+    }
+
+    public function update(TopicRequest $request, Topic $topic)
+    {
+        $this->authorize('update', $topic);
+
+        $topic->update($request->all());
         return new TopicResource($topic);
     }
 }
