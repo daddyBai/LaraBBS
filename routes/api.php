ac5c8b3ffd7d3e33953aa6cbc96b5c7e3b60b9e1
daddyBai <133122451@qq.com>
2020-05-15 04:16:58 +0000

    编辑用户信息

--- routes/api.php
+++ routes/api.php
@@ -22,37 +22,28 @@ Route::prefix('v1')
     ->namespace('Api')
     ->name('api.v1.')
     ->group(function() {
-
         Route::middleware('throttle:'.config('api.rate_limits.sign'))->group(function (){
-
             # 短信验证码
             Route::post('verificationCodes', 'VerificationCodesController@store')
                 ->name('verificationCodes.store');
-
             # 图片验证码
             Route::post('captchas', 'CaptchasController@store')->name('captchas.store');
-
             # 用户注册
             Route::post('users','UsersController@store')->name('users.store');
-
             # 第三方登录
             Route::post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
                 ->where('social_type','weixin')
                 ->name('socials.authorizations.store');
-
             # 登录
             Route::post('authorizations', 'AuthorizationsController@store')->name('api.authorizations.store');
-
             # 刷新 token
             Route::put('authorizations/current', 'AuthorizationsController@update')->name('authorizations.update');
-
             # 删除 token
             Route::delete('authorizations/current', 'AuthorizationsController@destroy')->name('authorizations.destroy');
         });
 
         # 游客可以访问的接口
         Route::middleware('throttle:'. config('api.rate_limits.access'))->group(function (){
-
             # 某个用户 详情
             Route::get('users/{user}', 'UsersController@show')->name('users.show');
 
@@ -60,6 +51,10 @@ Route::prefix('v1')
             Route::middleware('auth:api')->group(function (){
                 # 当前登录用户信息
                 Route::get('user', 'UsersController@me')->name('user.show');
+                # 编辑登录用户信息
+                Route::patch('user', 'UsersController@update')->name('user.update');
+                # 上传图片
+                Route::post('images', 'ImagesController@store')->name('images.store');
             });
         });
 
